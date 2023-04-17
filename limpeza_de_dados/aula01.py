4db786c60298f42c759a27f57be64e81b491ce11
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-17 20:14:08 -0300

    Data clear

---
+++ limpeza_de_dados/aula01.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# 4V da big data
+# Volume
+# Velocidade
+# Variedade
+# Veracidade
+
+######ENTENDER BEM OS DADOS######
+# Saber o objetivo
+# Se entrar lixo vai sair lixo
+# Transformar os dados em estruturas, listas, tuplas, dicionários, vetores, matrizes
+# Salvar em arquivos
+# Separar em diretórios
